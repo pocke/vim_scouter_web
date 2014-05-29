414ccfdb5685ce52a449dcbb2a07683228014852
pocke <p.ck.t22@gmail.com>
2014-05-30 07:26:07 +0900

    File upload

--- js/file.js
+++ js/file.js
@@ -1,6 +1,26 @@
 // Check for the various File API support.
 if (window.File && window.FileReader && window.FileList && window.Blob) {
-  // Great success! All the File APIs are supported.
+
+  var load_vimrc = function (file, callback) {
+    var reader = new FileReader();
+    // TODO: select file encoding
+    reader.onload = callback;
+
+    reader.readAsText(file, 'UTF-8');
+  };
+
+  $('#vimrc_upload').on('change', function (e) {
+    var input = $(this);
+    file = input.prop('files')[0];
+
+    load_vimrc(file, function (e) {
+      var vimrc_text = e.target.result;
+
+      vim_power = scouter(vimrc_text);
+      show_scouter(vim_power);
+    });
+  });
+
 } else {
   var upload_nav = $('#upload_nav');
   upload_nav.addClass('disabled');
--- js/scouter.js
+++ js/scouter.js
@@ -1,5 +1,11 @@
-
-
+// vim 戦闘力を計算して返す
 var scouter = function (vimrc) {
-  
+  // TODO: 一つ大きくなる?(末尾改行コードの影響?)
+  // TODO: 戦闘力を出す
+  return vimrc.split('\n').length;
+};
+
+// vim 戦闘力を表示
+var show_scouter = function (vim_power) {
+  console.log(vim_power);
 };
