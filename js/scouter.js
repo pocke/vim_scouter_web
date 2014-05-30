49074774ae1a86fe01a158695673f1f12e81c97a
pocke <p.ck.t22@gmail.com>
2014-05-30 23:26:40 +0900

    add Tweet button

--- js/scouter.js
+++ js/scouter.js
@@ -12,4 +12,10 @@ var show_scouter = function (vim_power) {
   var text = 'あなたのVim戦闘力は<strong>' + vim_power + '</strong>です。';
   var tag = $('<h3>').html(text);
   $('#result').html(tag);
+
+
+  //  for tweet button
+  var tweet_btn = $('#tweet_btn');
+  var url = 'https://twitter.com/intent/tweet?original_referer=&text=' + encodeURIComponent('私のVim戦闘力は' + vim_power + 'です。') + '&tw_p=tweetbutton&url=http%3A%2F%2Fpocke.github.io%2Fvim_scouter_web%2F';
+  tweet_btn.attr('href', url);
 };
