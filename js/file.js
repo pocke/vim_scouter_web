02e55a725813194d398cd1a23965aae4168dcd2c
pocke <p.ck.t22@gmail.com>
2014-05-30 18:37:27 +0900

    file local scope

--- js/file.js
+++ js/file.js
@@ -1,28 +1,36 @@
-// Check for the various File API support.
-if (window.File && window.FileReader && window.FileList && window.Blob) {
+(function () {
 
-  var load_vimrc = function (file, callback) {
-    var reader = new FileReader();
-    // TODO: select file encoding
-    reader.onload = callback;
 
-    reader.readAsText(file, 'UTF-8');
-  };
 
-  $('#vimrc_upload').on('change', function (e) {
-    var input = $(this);
-    file = input.prop('files')[0];
+  // Check for the various File API support.
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
 
-    load_vimrc(file, function (e) {
-      var vimrc_text = e.target.result;
+    var load_vimrc = function (file, callback) {
+      var reader = new FileReader();
+      // TODO: select file encoding
+      reader.onload = callback;
 
-      vim_power = scouter(vimrc_text);
-      show_scouter(vim_power);
+      reader.readAsText(file, 'UTF-8');
+    };
+
+    $('#vimrc_upload').on('change', function (e) {
+      var input = $(this);
+      file = input.prop('files')[0];
+
+      load_vimrc(file, function (e) {
+        var vimrc_text = e.target.result;
+
+        vim_power = scouter(vimrc_text);
+        show_scouter(vim_power);
+      });
     });
-  });
 
-} else {
-  var upload_nav = $('#upload_nav');
-  upload_nav.addClass('disabled');
-  upload_nav.children('a').attr('data-toggle', '');
-}
+  } else {
+    var upload_nav = $('#upload_nav');
+    upload_nav.addClass('disabled');
+    upload_nav.children('a').attr('data-toggle', '');
+  }
+
+
+
+})();
